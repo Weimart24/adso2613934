641a0c638fa186d10b73926dd97a120dfa34e868
weimart24 <weimart24@gmail.com>
2024-02-20 21:18:03 -0500

    Se creó varios archivos y se dió introduccion a poo

--- 01-oop/01-introduction.php
+++ 01-oop/01-introduction.php
@@ -1,8 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>01- Introduction</title>
+<link rel="stylesheet" href="css/master.css">
+</head>
+
+<body>
+    <main>
+        <form action="" method="post">
+            <label>
+                <p>Number 1:</p>
+                <input type="range" name="n1" step="1" value="0" oninput="o1.value=this.value">
+                <output id="o1">0</output>
+            </label>
+
+            <label>
+                <p>Number 2:</p>
+                <input type="range" name="n2" step="1" value="0" oninput="o2.value=this.value">
+                <output id="o2">0</output>
+            </label>
+            <button> Calc</button>
+        </form>
+        <div class="result">
+            <?php
+            if ($_POST) {
+                $sum = new Adition();
+                $sum->num1 = $_POST['n1'];
+                $sum->num2 = $_POST['n2'];
+                echo "<br>La suma de {$sum->num1} y {$sum->num2} es: " . $sum->getResult();
+            }
+            ?>
+        </div>
+    </main>
+</body>
+
+</html>
+
 <?php
 # Linar Programing
 $num1 = 54;
 $num2 = 32;
 
+echo "<br>";
 echo "{$num1} * {$num2} = " . ($num1 * $num2);
 echo "<br>";
 
@@ -15,8 +57,39 @@ echo "<br>";
 
 $hash = password_hash($string, PASSWORD_DEFAULT);
 
-if(password_verify($string, $hash)){
+if (password_verify($string, $hash)) {
     echo "Verifed Successful";
 }
+
+
+# Structured Programming
+
+function adition($num1, $num2 = 7)
+{
+    return ($num1 + $num2);
+}
+
+$rs = adition(34, 890);
+echo "<br>" . $rs . "<br>";
+$rs = adition(89);
+echo $rs;
+
+
+# Object Oriented Programming
+
+class Adition
+{
+    public $num1;
+    public $num2;
+
+    public function getResult()
+    {
+        return ($this->num1 + $this->num2); # hay que fijarse en los signos $ al momento de llamar a las variables
+    }
+}
+$sum = new Adition();
+$sum->num1 = 1024;
+$sum->num2 = 512;
+echo "<br>La suma de {$sum->num1} y {$sum->num2} es: " . $sum->getResult();
 
 ?>
---
+++ 01-oop/02-fundamentals.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>01- Introduction</title>
+    <link rel="stylesheet" href="css/master.css">
+    <style>
+        section {
+            background-color: #0009;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            gap: 1rem;
+            padding: 10px;
+
+            h2 {
+                margin: 0;
+            }
+            figure {
+                font-size: 4rem;
+            }
+        }
+    </style>
+</head>
+
+<body>
+    <main>
+        <h1>02- Fundamentals</h1>
+        <section>
+            <figure>
+                <?php
+                class Runner
+                {
+                    // Attributes
+                    private $name;
+                    private $age;
+                    private $number;
+
+                    // Methods
+                    public function __construct($name, $age, $number)
+                    {
+                        $this->name   = $name;
+                        $this->age    = $age;
+                        $this->number = $number;
+                    }
+
+                    public function run()
+                    {
+                        return "🏃";
+                    }
+
+                    public function stop()
+                    {
+                        return "🧍";
+                    }
+
+                    public function jump()
+                    {
+                        return "🦘";
+                    }
+                }
+
+                $runner = new Runner('Weimar', '32', '91');
+                echo $runner->run();
+                echo $runner->jump();
+                echo $runner->stop();
+                echo $runner->run();
+
+                ?>
+            </figure>
+        </section>
+    </main>
+</body>
+
+</html>
