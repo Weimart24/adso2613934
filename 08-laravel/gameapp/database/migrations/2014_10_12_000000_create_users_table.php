a40f133fd385f0192b2c35639026e1ad6c5686d5
Weimart24 <weimart24@gmail.com>
2024-06-13 21:15:55 -0500

    Se trabajó la base de datos con laravel

--- 08-laravel/gameapp/database/migrations/2014_10_12_000000_create_users_table.php
+++ 08-laravel/gameapp/database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,7 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->bigInteger('document')->unique();
+            $table->string('fullname');
+            $table->date('birthdate');
+            $table->string('photo')->default('no-photo.jpg');
+            $table->string('phone');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
